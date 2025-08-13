d80490aa6d349c793292b6772859671affdd39c6
Radcabna <radcabna@gmail.com>
2025-08-13 16:02:36 +0300

    Initial Commit

---
+++ Two Kings/Two_KingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Two_KingsApp.swift
+//  Two Kings
+//
+//  Created by Алкександр Степанов on 13.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Two_KingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
